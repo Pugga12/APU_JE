ddf80932446285f0bc3910b99e0af8f8baf133b7
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-05-19 17:42:59 -0400

    Move all  listeners off the project As of build e5, the traditional listeners of all commands will be have to be moved off the repository to add slash commands. The easter egg commands will be dealt with separately I have already started work on the info command

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # APU_JE/Experimental Branch
 A modular discord bot made in JDA 
 This branch is to implement new features from pre-releases of JDA without affecting stable
-After experimental features become stable, they will be merged onto the master branch
+After experimental features become stable, they will be merged onto the master branch manually\
+No pull requests will be made due to the inconsistencies of the UI between branches.
 # UI
 ![HELP](https://cdn.discordapp.com/attachments/804865488181788755/834901465586991104/unknown.png)
--- src/main/java/com/notaddypug/apu/core/botcore.java
+++ src/main/java/com/notaddypug/apu/core/botcore.java
@@ -1,12 +1,10 @@
 package com.notaddypug.apu.core;
 
-import com.notaddypug.apu.functions.CmdHelpCmdListener;
 import com.notaddypug.apu.functions.WishYouLuckCmdListener;
-import com.notaddypug.apu.functions.games.gameSelect_error;
-import com.notaddypug.apu.functions.info;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.utils.SessionController;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.requests.restaction.CommandUpdateAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,11 +16,12 @@ public class botcore {
         Logger logger = LoggerFactory.getLogger(botcore.class);
         logger.info("Instance is now launching! Due to sharding, loading may take a while!");
         JDABuilder shardBuilder = JDABuilder.createDefault(CfgHandler.get("token"));
-        shardBuilder.addEventListeners(new CmdHelpCmdListener());
         shardBuilder.addEventListeners(new WishYouLuckCmdListener());
-        shardBuilder.addEventListeners(new gameSelect_error());
-        shardBuilder.addEventListeners(new info());
         shardBuilder.setActivity(Activity.playing("apu!help for help | Experimental Build"));
+        CommandUpdateAction cmds = shardBuilder.build().updateCommands();
+        cmds.addCommands(
+                new CommandData("info", "Get info about this bot"));
+        cmds.queue();
         logger.info("Beginning Sharding!");
         for (int i = 0; i < 3; i++)
 
