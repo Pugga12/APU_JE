17debc49c7e9a97b9fc0f9560bfe13b5c7b23585
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-10 17:54:27 -0400

    Complete Removal Of Old Listeners

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# APU_JE/Experimental Branch
+# APU_JE/experimental Branch
 A modular discord bot made in JDA 
 This branch is to implement new features from pre-releases of JDA without affecting stable
 After experimental features become stable, they will be merged onto the master branch manually\
 No pull requests will be made due to the inconsistencies of the UI between branches.
+## Branch Status
+Code in this branch is currently STABLE
 # UI
-![HELP](https://cdn.discordapp.com/attachments/804865488181788755/834901465586991104/unknown.png)
+![Available Commands](https://cdn.discordapp.com/attachments/804865488181788755/850731905837170698/unknown.png)
--- src/main/java/com/addypug/apu/core/main.java
+++ src/main/java/com/addypug/apu/core/main.java
@@ -1,6 +1,7 @@
-package com.notaddypug.apu.core;
+package com.addypug.apu.core;
 
-import com.notaddypug.apu.functions.WishYouLuckCmdListener;
+
+import com.addypug.apu.wrap.Meta;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDAInfo;
@@ -9,33 +10,31 @@ import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.requests.restaction.CommandUpdateAction;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.notaddypug.apu.wrap.Meta;
+
 import java.awt.*;
 
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
-public class botcore extends ListenerAdapter {
-    public static void main(String[] arguments) throws Exception
-    {
-        System.out.println("Build Info: Version " + Meta.version + " (" + Meta.stability + ", Built on JDA "  + JDAInfo.VERSION + ") on branch " + Meta.branch);
+public class main extends ListenerAdapter {
+    public static void main(String[] arguments) throws Exception {
+        Logger logger = LoggerFactory.getLogger(main.class);
+        System.out.println("Build Info: Version " + Meta.version + "_" + Meta.build + " (" + Meta.stability + ", Built on JDA " + JDAInfo.VERSION + ") @ branch " + Meta.branch);
         System.out.println(Meta.stability_msg);
-        Logger logger = LoggerFactory.getLogger(botcore.class);
         logger.info("Instance is now launching! Due to sharding, loading may take a while!");
-        JDABuilder shardBuilder = JDABuilder.createDefault(CfgHandler.get("token"));
-        shardBuilder.addEventListeners(new botcore());
-        shardBuilder.addEventListeners(new WishYouLuckCmdListener());
+        String token = CfgHandler.valString("token");
+        JDABuilder shardBuilder = JDABuilder.createDefault(token);
+        shardBuilder.addEventListeners(new main());
         shardBuilder.setActivity(Activity.playing("Type / to see available Commands | APU Experimental " + Meta.version));
-        int shardinteger = Integer.parseInt(CfgHandler.get("shardint"));
+        Integer shardinteger = CfgHandler.valInt("shardint");
         logger.info("Beginning Sharding! Shards to initialize: " + shardinteger);
         for (int i = 0; i < shardinteger; i++)
-
             shardBuilder.useSharding(i, shardinteger)
                     .build();
         logger.info("Sharding complete!");
-        CommandUpdateAction cmds = shardBuilder.build().updateCommands();
+        CommandListUpdateAction cmds = shardBuilder.build().updateCommands();
         cmds.addCommands(
                 new CommandData("info", "Get info about this bot")
         );
@@ -44,18 +43,22 @@ public class botcore extends ListenerAdapter {
                         .addOptions(new OptionData(STRING, "egg", "The code to activate the egg")
                                 .setRequired(true))
         );
+        logger.info("Attempting to synchronize slash commands");
         cmds.queue();
     }
+
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
-        if (event.getGuild() == null) return;
-        if (event.getName().equals("info")) {
+        // Only accept commands from guilds
+        if (event.getGuild() == null)
+            return;
+        switch (event.getName()) {
+            case "info":
                 EmbedBuilder ebd = new EmbedBuilder();
                 ebd.setColor(Color.red);
                 ebd.addField("About This Bot", "APU is a modular discord bot made in JDA", true);
                 ebd.addField("Support", "Source Code & Updates (for experimental branch): https://github.com/Pugga12/APU_JE/tree/experimental_features\nReport Issues: https://github.com/Pugga12/APU_JE/issues\nPrivacy Policy and Other Legal Documents: https://www.addypug.com/projects/apu/legal", true);
                 ebd.setFooter("AddyPug's Utilities Experimental " + Meta.version + "\n" + Meta.stability_msg);
-                event.replyEmbeds(ebd.build()).setEphemeral(false).queue();
         }
     }
 }
