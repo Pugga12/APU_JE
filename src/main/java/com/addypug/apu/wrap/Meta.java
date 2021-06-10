17f6f4810b91e3191fc149c4e9f26000e1c48598
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-10 16:55:23 -0400

    Cosmetics Update 2
    This restyles the logger. Continuation of 0511a693

---
+++ src/main/java/com/addypug/apu/wrap/Meta.java
@@ -0,0 +1,13 @@
+package com.addypug.apu.wrap;
+
+public class Meta {
+    /**
+     * The Meta Class Contains Information About This APU Build, like the JDAInfo class in JDA
+     */
+    public static final String branch = "experimental";
+    public static final String stability = "Stable"; //
+    public static final String version = "0.2.6";
+    public static final String build = "pre1.4";
+    public static final String stability_msg = ""; // this should be uncommented if the current build uses stable versions of JDA
+    //public static final String stability_msg = "This build is experimental and may be unstable. Please tag your issues with branch:@experimental_features if a bug is found"; // this should be uncommented if the current build uses experimental versions of JDA
+}
