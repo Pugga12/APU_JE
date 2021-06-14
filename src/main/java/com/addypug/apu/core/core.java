8c1af5bf633e276a3c3ab11eaafbc45f5c57d52e
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-14 10:49:48 -0400

    Delete runoff of merge conflict fix

--- src/main/java/com/addypug/apu/core/core.java
+++ src/main/java/com/addypug/apu/core/core.java
@@ -1,32 +1,28 @@
 package com.addypug.apu.core;
 
 
+import com.addypug.apu.fn.infocmd;
 import com.addypug.apu.wrap.Meta;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
-public class main extends ListenerAdapter {
+public class core {
     public static void main(String[] arguments) throws Exception {
-        Logger logger = LoggerFactory.getLogger(main.class);
+        Logger logger = LoggerFactory.getLogger(core.class);
         System.out.println("Build Info: Version " + Meta.version + "_" + Meta.build + " (" + Meta.stability + ", Built on JDA " + JDAInfo.VERSION + ") @ branch " + Meta.branch);
         System.out.println(Meta.stability_msg);
         logger.info("Instance is now launching! Due to sharding, loading may take a while!");
         String token = CfgHandler.valString("token");
         JDABuilder shardBuilder = JDABuilder.createDefault(token);
-        shardBuilder.addEventListeners(new main());
+        shardBuilder.addEventListeners(new infocmd());
         shardBuilder.setActivity(Activity.playing("Type / to see available Commands | APU Experimental " + Meta.version));
         Integer shardinteger = CfgHandler.valInt("shardint");
         logger.info("Beginning Sharding! Shards to initialize: " + shardinteger);
@@ -45,20 +41,5 @@ public class main extends ListenerAdapter {
         );
         logger.info("Attempting to synchronize slash commands");
         cmds.queue();
-    }
-
-    @Override
-    public void onSlashCommand(SlashCommandEvent event) {
-        // Only accept commands from guilds
-        if (event.getGuild() == null)
-            return;
-        switch (event.getName()) {
-            case "info":
-                EmbedBuilder ebd = new EmbedBuilder();
-                ebd.setColor(Color.red);
-                ebd.addField("About This Bot", "APU is a modular discord bot made in JDA", true);
-                ebd.addField("Support", "Source Code & Updates (for experimental branch): https://github.com/Pugga12/APU_JE/tree/experimental_features\nReport Issues: https://github.com/Pugga12/APU_JE/issues\nPrivacy Policy and Other Legal Documents: https://www.addypug.com/projects/apu/legal", true);
-                ebd.setFooter("AddyPug's Utilities Experimental " + Meta.version + "\n" + Meta.stability_msg);
-        }
     }
 }
---
+++ src/main/java/com/addypug/apu/fn/infocmd.java
@@ -0,0 +1,6 @@
+package com.addypug.apu.fn;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class infocmd extends ListenerAdapter {
+}
