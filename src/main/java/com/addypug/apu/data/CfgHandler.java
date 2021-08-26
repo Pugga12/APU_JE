54397ede6c2513bd768585f328ea95fa85a59804
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-08-25 20:29:43 -0400

    Prep For Merge Into Stable

--- src/main/java/com/addypug/apu/data/CfgHandler.java
+++ src/main/java/com/addypug/apu/data/CfgHandler.java
@@ -1,6 +1,8 @@
 package com.addypug.apu.data;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.annotations.DeprecatedSince;
+import net.dv8tion.jda.annotations.ForRemoval;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,17 +10,14 @@ import org.slf4j.LoggerFactory;
 public class CfgHandler {
     static Logger logger = LoggerFactory.getLogger(CfgHandler.class);
     private static final Dotenv dotenv = Dotenv.load();
-
     public static String valString(String key) {
         logger.debug("Retrieving String value from .env file with key " + key);
         return dotenv.get(key);
     }
-
     public static Boolean valBool(String key) {
         logger.debug("Retrieving Boolean value from .env file with key " + key);
         return Boolean.parseBoolean(dotenv.get(key));
     }
-
     public static Integer valInt(String key) {
         logger.debug("Retrieving Integer value from .env file with key " + key);
         return Integer.parseInt(dotenv.get(key));
--- src/main/java/com/addypug/apu/data/values.java
+++ src/main/java/com/addypug/apu/data/values.java
@@ -5,11 +5,11 @@ public class values {
      * The Values Class Contains Information About This APU Build, like the JDAInfo class in JDA
      */
     public static final String update_server_endpoint = "https://api.npoint.io/280f90167ea69341987e";
-    public static final String branch = "dev/cappuccino";
-    public static final String stability = "Snapshot"; //
-    public static final String version = "21w1a";
-    public static final String release_json = "\"21w1a\"";
+    public static final String branch = "Stable";
+    public static final String stability = "Stable"; //
+    public static final String version = "0.2.6_68";
+    public static final String release_json = "\"0.2.6_68\"";
     public static final String database_revision = "DSR8.5";
     public static final String stability_msg = ""; // this should be uncommented if the current build uses stable versions of JDA
-    public static final String release_status = "Snapshot";
+    public static final String release_status = "Alpha";
 }
