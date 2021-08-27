Pugga12/APU_JE

commit ac297030441ddef80265b3303a95f5b93d77fe7a

Add Join Command
The join command allows the music bot to join a vc

src/main/java/com/addypug/apu/commands/adminutils/shutdown.java
@@ -1,0 +1,24 @@
+package com.addypug.apu.commands.adminutils;
+
+import com.addypug.apu.data.CfgHandler;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+
+public class shutdown extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+        if (event.getMessage().getContentRaw().equals("dev!shutdown")) {
+            if (event.getAuthor().getId().equals(CfgHandler.valString("bot_owner"))) {
+                EmbedBuilder ebd = new EmbedBuilder();
+                ebd.setColor(Color.green);
+                ebd.addField("Bot is shutting down", "Shutdown command was sent", true);
+                event.getMessage().getChannel().sendMessageEmbeds(ebd.build()).queue();
+                event.getJDA().shutdownNow();
+            }
+        }
+    }
+}

.circleci/config.yml
@@ -31,13 +31,13 @@ jobs:
             # fallback to using the latest cache if no exact match is found
             - v1-dependencies-
 
-      - run: gradle buildDependents
       - run:
-          name: 'Set Version'
-          command: release-version=$(gradle release-version)
+          name: 'Build JAR'
+          command: gradle buildDependents
+
       - save_cache:
           paths:
             - ~/.gradle
           key: v1-dependencies-{{ checksum "build.gradle" }}
       - store_artifacts:
-              path: build/libs/APU-${release-version}-all.jar
+          path: build/libs

build.gradle
@@ -4,7 +4,7 @@ plugins {
     id'com.github.johnrengelman.shadow' version '5.2.0'
 }
 mainClassName = 'com.addypug.apu.core'
-version '0.2.6_68'
+version '0.2.7_10'
 
 sourceCompatibility = targetCompatibility = 1.8
 
@@ -16,11 +16,11 @@ repositories {
 
 dependencies {
     implementation ('net.dv8tion:JDA:4.3.0_310')
-    implementation("ch.qos.logback:logback-classic:1.2.4-groovyless")
+    implementation('ch.qos.logback:logback-classic:1.2.5')
     implementation (group: 'io.github.cdimascio', name: 'java-dotenv', version: '5.2.2')
     implementation ('com.google.code.gson:gson:2.8.8')
     implementation ('com.sedmelluq:lavaplayer:1.3.78')
-    implementation ('com.zaxxer:HikariCP:4.0.3')
+    implementation ('com.zaxxer:HikariCP:5.0.0')
     implementation ('org.xerial:sqlite-jdbc:3.36.0.1')
 }
 jar {

src/main/java/com/addypug/apu/ShardingTools.java
@@ -1,16 +1,0 @@
-package com.addypug.apu;
-
-import net.dv8tion.jda.api.JDA;
-
-/**
- * ShardingTools contains toolsets for sgarding
- */
-public class ShardingTools {
-    /**
-     *
-     * @param jda The JDA instance
-     */
-    public static int getNonZeroIndexedShardId(JDA jda) {
-        return jda.getShardInfo().getShardId() + 1;
-    }
-}

src/main/java/com/addypug/apu/commands/adminutils/banUser.java
@@ -1,4 +1,4 @@
-package com.addypug.apu.functions.adminutils;
+package com.addypug.apu.commands.adminutils;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;

src/main/java/com/addypug/apu/commands/adminutils/kickUser.java
@@ -1,4 +1,4 @@
-package com.addypug.apu.functions.adminutils;
+package com.addypug.apu.commands.adminutils;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -11,36 +11,46 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 
-public class unbanUser extends ListenerAdapter {
-    Logger logger = LoggerFactory.getLogger(unbanUser.class);
+public class kickUser extends ListenerAdapter {
+    Logger logger = LoggerFactory.getLogger(kickUser.class);
+
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
-        if (event.getName().equals("unban")) {
+        if (event.getGuild() == null) return;
+        if (event.getName().equals("kick")) {
             User user = event.getOption("user").getAsUser();
             Member member = event.getOption("user").getAsMember();
             logger.debug("Valid command received");
             EmbedBuilder ebd = new EmbedBuilder();
             event.deferReply().queue();
-            if (!event.getMember().hasPermission(Permission.BAN_MEMBERS)) {
+            if (!event.getMember().hasPermission(Permission.KICK_MEMBERS)) {
                 ebd.setTitle("Error: Access Is Denied");
-                ebd.addField("Insufficient Permissions", "You do not have the permission to perform this command\nRequires Permission: Ban User (Code P415)", true);
+                ebd.addField("Insufficient Permissions", "You do not have the permission to perform this command\nRequires Permission: Kick User (Code P415)", true);
                 ebd.setColor(Color.blue);
                 ebd.setFooter("Please do not file a issue for this error. It will be closed");
                 event.getHook().editOriginalEmbeds(ebd.build()).queue();
             } else {
                 Member selfMember = event.getGuild().getSelfMember();
-                if (!selfMember.hasPermission(Permission.BAN_MEMBERS)) {
+                if (!selfMember.hasPermission(Permission.KICK_MEMBERS)) {
                     ebd.setTitle("Error: Access Is Denied");
-                    ebd.addField("Insufficient Permissions", "I do not have the permission to unban this member\nRequired Permission: Ban User (Code P415-2)", true);
+                    ebd.addField("Insufficient Permissions", "I do not have enough permissions to kick this member\nRequired Permission: Kick User (Code P415-2)", true);
                     ebd.setColor(Color.blue);
                     ebd.setFooter("Please do not file a issue for this error. It will be closed");
                     return;
                 }
+                if (member != null && !selfMember.canInteract(member)) {
+                    ebd.setTitle("Error: Access Is Denied");
+                    ebd.addField("Cannot Interact With This User", "I cannot interact with this user\n User is too powerful for me to kick (Code P416)", true);
+                    ebd.setColor(Color.blue);
+                    ebd.setFooter("Please do not file a issue for this error. It will be closed");
+                    event.getHook().editOriginalEmbeds(ebd.build()).queue();
+                    return;
+                }
                 ebd.setTitle("Action Completed!");
-                ebd.addField("A user was unbanned successfully", user.getAsMention() + " was unbanned", true);
+                ebd.addField("A user was kicked successfully", user.getAsMention() + " was kicked from the server", true);
                 ebd.setColor(Color.red);
                 event.getHook().editOriginalEmbeds(ebd.build()).queue();
-                event.getGuild().unban(user).queue();
+                event.getGuild().kick(member).queue();
             }
         }
     }

src/main/java/com/addypug/apu/commands/adminutils/unbanUser.java
@@ -1,4 +1,4 @@
-package com.addypug.apu.functions.adminutils;
+package com.addypug.apu.commands.adminutils;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -11,46 +11,36 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 
-public class kickUser extends ListenerAdapter {
-    Logger logger = LoggerFactory.getLogger(kickUser.class);
-
+public class unbanUser extends ListenerAdapter {
+    Logger logger = LoggerFactory.getLogger(unbanUser.class);
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
-        if (event.getGuild() == null) return;
-        if (event.getName().equals("kick")) {
+        if (event.getName().equals("unban")) {
             User user = event.getOption("user").getAsUser();
             Member member = event.getOption("user").getAsMember();
             logger.debug("Valid command received");
             EmbedBuilder ebd = new EmbedBuilder();
             event.deferReply().queue();
-            if (!event.getMember().hasPermission(Permission.KICK_MEMBERS)) {
+            if (!event.getMember().hasPermission(Permission.BAN_MEMBERS)) {
                 ebd.setTitle("Error: Access Is Denied");
-                ebd.addField("Insufficient Permissions", "You do not have the permission to perform this command\nRequires Permission: Kick User (Code P415)", true);
+                ebd.addField("Insufficient Permissions", "You do not have the permission to perform this command\nRequires Permission: Ban User (Code P415)", true);
                 ebd.setColor(Color.blue);
                 ebd.setFooter("Please do not file a issue for this error. It will be closed");
                 event.getHook().editOriginalEmbeds(ebd.build()).queue();
             } else {
                 Member selfMember = event.getGuild().getSelfMember();
-                if (!selfMember.hasPermission(Permission.KICK_MEMBERS)) {
+                if (!selfMember.hasPermission(Permission.BAN_MEMBERS)) {
                     ebd.setTitle("Error: Access Is Denied");
-                    ebd.addField("Insufficient Permissions", "I do not have enough permissions to kick this member\nRequired Permission: Kick User (Code P415-2)", true);
+                    ebd.addField("Insufficient Permissions", "I do not have the permission to unban this member\nRequired Permission: Ban User (Code P415-2)", true);
                     ebd.setColor(Color.blue);
                     ebd.setFooter("Please do not file a issue for this error. It will be closed");
-                    return;
-                }
-                if (member != null && !selfMember.canInteract(member)) {
-                    ebd.setTitle("Error: Access Is Denied");
-                    ebd.addField("Cannot Interact With This User", "I cannot interact with this user\n User is too powerful for me to kick (Code P416)", true);
-                    ebd.setColor(Color.blue);
-                    ebd.setFooter("Please do not file a issue for this error. It will be closed");
-                    event.getHook().editOriginalEmbeds(ebd.build()).queue();
                     return;
                 }
                 ebd.setTitle("Action Completed!");
-                ebd.addField("A user was kicked successfully", user.getAsMention() + " was kicked from the server", true);
+                ebd.addField("A user was unbanned successfully", user.getAsMention() + " was unbanned", true);
                 ebd.setColor(Color.red);
                 event.getHook().editOriginalEmbeds(ebd.build()).queue();
-                event.getGuild().kick(member).queue();
+                event.getGuild().unban(user).queue();
             }
         }
     }

src/main/java/com/addypug/apu/commands/check_my_permissions.java
@@ -2,7 +2,7 @@
  * Permission Checker
  * Checks the permission levels of users
  */
-package com.addypug.apu.functions;
+package com.addypug.apu.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;

commit ffe2104746b859bd9e548eb2efcfe4c324a246de

Execute Queued Rest Actions Before Shutdown
This fixes the bug introduced in ac297030

src/main/java/com/addypug/apu/commands/adminutils/shutdown.java
@@ -17,7 +17,7 @@ public class shutdown extends ListenerAdapter {
                 ebd.setColor(Color.green);
                 ebd.addField("Bot is shutting down", "Shutdown command was sent", true);
                 event.getMessage().getChannel().sendMessageEmbeds(ebd.build()).queue();
-                event.getJDA().shutdownNow();
+                event.getJDA().shutdown();
             }
         }
     }