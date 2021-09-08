3d9f1bf148c14ebdf5e031ba4fb4db52036ebe16
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-09-08 08:12:00 -0400

    [Fix Vulnerability] SQL Injection In guildDb.java
    This solves the SQLI vulnerability in guildDb.java, where it did not sanitize special characters

--- src/main/java/com/addypug/apu/data/dbsubst/guildDb.java
+++ src/main/java/com/addypug/apu/data/dbsubst/guildDb.java
@@ -1,14 +1,36 @@
+/*
+  Copyright 2021 NotAddyPug
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 package com.addypug.apu.data.dbsubst;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class guildDb {
     /**
      * Enters a server into the guild_settings table
-     * @param id The ID of the server
-     * @throws SQLException
+     * @param id The ID of the guild
      */
+
     public static void createServerRow(String id) throws SQLException {
-        SQLiteDataSource.getConnection().createStatement().execute("INSERT or IGNORE into guild_settings(guildId) VALUES('" + id + "')");
+        Connection connection = SQLiteDataSource.getConnection();
+        String query = "INSERT or IGNORE into guild_settings(guildId) values(?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, id);
+        statement.executeUpdate();
     }
 }
